89ae9fc29148d9d6b9588cc95eda0a4563e1d4b8
leo-guinan <leo@copy.ai>
2022-10-18 10:29:41 -0400

    fixing admin add feed functionality

--- app/admin/mutations/addFeedToAccount.ts
+++ app/admin/mutations/addFeedToAccount.ts
@@ -32,21 +32,48 @@ export default async function addFeedToAccount(
       feedType = FeedType.BLOG
       break
   }
+  const organization = await db.organization.findFirst({
+    where: {
+      membership: {
+        some: {
+          userId: userId,
+        }
+      }
+    }
+
+  });
+  if (!organization) {
+    throw new Error("Organization not found")
+  }
+  const garden = await db.garden.findFirst({
+    where: { ownerId: organization.id },
+  })
+  if (!garden) {
+    throw new Error("Garden not found")
+  }
   const feed = await db.feed.upsert({
     where: { feed: feedUrl },
     create: {
       feed: feedUrl,
       type: feedType,
-      savedBy: {
-        connect: { id: userId }
-      }
+      gardens: {
+        connect: {
+          id: garden.id
+        }
+      },
+
     },
     update: {
-      savedBy: {
-        connect: { id: userId }
-      }
+      type: feedType,
+      gardens: {
+        connect: {
+          id: garden.id
+        }
+      },
     }
   })
+
+  console.log(feed)
   const user = await db.user.findFirst({
     where: { id: userId }
   })
